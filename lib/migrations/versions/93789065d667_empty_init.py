aa654984cf444d006798806d420df3fe17f346f8
Chen <chen.jiang.1206@gmail.com>
2023-05-11 13:12:34 -0400

    practice

---
+++ lib/migrations/versions/93789065d667_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 93789065d667
+Revises: 
+Create Date: 2023-05-11 13:01:38.394626
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '93789065d667'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
